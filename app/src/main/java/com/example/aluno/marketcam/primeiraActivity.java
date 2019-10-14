132813e1275028ff3eddf940b7c25febe8c89ff8
Rafael Aranda Martins <skema1114@hotmail.com>
2019-10-14 17:27:56 -0300

    versao 6

--- app/src/main/java/com/example/aluno/marketcam/primeiraActivity.java
+++ app/src/main/java/com/example/aluno/marketcam/primeiraActivity.java
@@ -1,12 +1,13 @@
 package com.example.aluno.marketcam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 
-public class primeiraActivity extends AppCompatActivity implements View.OnClickListener{
+public class primeiraActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnJCadastroT1;
     private Button btnNCadastroT1;
 
@@ -17,18 +18,25 @@ public class primeiraActivity extends AppCompatActivity implements View.OnClickL
 
          /*findViewById*/
          /*findViewById dos Button*/
-         btnJCadastroT1 = (Button) findViewById(R.id.btnJCadastroT1);
-         btnNCadastroT1 = (Button) findViewById(R.id.btnNCadastroT1);
+        btnJCadastroT1 = (Button) findViewById(R.id.btnJCadastroT1);
+        btnNCadastroT1 = (Button) findViewById(R.id.btnNCadastroT1);
 
          /*setOnClickListener*/
          /*setOnClickListener dos Button*/
-         btnJCadastroT1.setOnClickListener(this);
-         btnNCadastroT1.setOnClickListener(this);
+        btnJCadastroT1.setOnClickListener(this);
+        btnNCadastroT1.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-
+        if (btnJCadastroT1.isPressed()) {
+            Intent intent = new Intent(this, terceiraActivity.class);
+            startActivity(intent);
+        }
+        if (btnNCadastroT1.isPressed()) {
+            Intent intent = new Intent(this, segundaActivity.class);
+            startActivity(intent);
+        }
     }
 }
